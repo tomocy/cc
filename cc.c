2086fb7b34c788306a1e78e218c2873ad8f6860d
tomocy <tomocy.dev@gmail.com>
2018-12-19 18:35:04 +0900

    change format style to Google with indent space 4

--- cc.c
+++ cc.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
-{
-    if (argc != 2)
-    {
+int main(int argc, char** argv) {
+    if (argc != 2) {
         fprintf(stderr, "wrong number of parameters\n");
     }
 
-    char *arg = argv[1];
+    char* arg = argv[1];
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
     printf("    mov rax, %1d\n", strtol(arg, &arg, 10));
 
-    while (*arg)
-    {
-        if (*arg == '+')
-        {
+    while (*arg) {
+        if (*arg == '+') {
             arg++;
             printf("    add rax, %1d\n", strtol(arg, &arg, 10));
             continue;
         }
 
-        if (*arg == '-')
-        {
+        if (*arg == '-') {
             arg++;
             printf("    sub rax, %1d\n", strtol(arg, &arg, 10));
             continue;
